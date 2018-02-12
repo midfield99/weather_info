c21689d025863df9d251f33f97cf9f6a07e4573d
Chris Matthews <midfield_3@hotmail.com>
2018-02-12 10:36:22 -0600

    added a prompt, output, and more validation.

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ import os
 import requests
 
 def validate_json_response(res):
-    code = str(res.get('cod')) #I've seen both number response codes and string response code from this api.
+    #I've seen both number response codes and string response code from this api.
+    code = str(res.get('cod'))
     if code != '200':
         err = "Api call unsuccessful. Code: {} Error Message: {}"
         raise LookupError(err.format(code, res.get("message")))
+    elif res.get('main', {}).get('temp') is None:
+        raise LookupError("Temperature was not found in response.")
 
+#Rate limiting should definitely be looked at if this was going to be used.
+# But I wouldn't expect excessive API usage with a simple command prompt program.
 def get_api_response(city):
     api_key = os.environ.get('OWM_API_KEY')
     base_url = os.environ.get('OWM_BASE_URL')
@@ -15,8 +20,46 @@ def get_api_response(city):
         err = "Environmental variable retrieval failed. Was the API key set and config file sourced?"
         raise SystemExit(err)
 
-    url = base_url + 'weather?q={}&appid={}'.format(city, api_key)
+    url = base_url + 'weather?q={}&appid={}&units=imperial'.format(city, api_key)
     response = requests.get(url).json()
+
     validate_json_response(response)
 
     return response
+
+
+#Note, this validation approach isn't entirely accurate.
+# It should allow nearly all cities,
+# but I have found some cities with special characters.
+# But this method is simple.
+#
+# I also checked a couple cities with abbreviations (like Ft. Worth)
+# and it looks like the api returns 404s with some common abbreviations.
+# So I'm ok not accepting abbreviations or periods.
+#
+# I'm also not implementing spell check.
+def validate_input(city):
+    if not city.replace(' ','').isalpha():
+        msg = "City is invalid. Please only use letters and spaces in city name."
+        raise ValueError(msg)
+
+#Note, the state is intentionally ignored.
+# I've checked the API documentation and it doesn't look like there is an endpoint for a city/state pair.
+# Just city/country. This will cause issues, for instance a lot of states have a city Riverside.
+#
+# I've also checked temps on the site via the search bar, multiple results can be returned,
+# but again it looks like adding a state does not change ordering. Riverside,Iowa and Riverside,California
+# both returned the same results in the same order.
+# It looks like the api endpoint just returns the first item in that list.
+# 
+# using a zip code to retrieve temperatures would probably be much more accurate.
+def retrieve_temp():
+    city = input("What city are you?\n")
+    validate_input(city)
+    res = get_api_response(city)
+    temp = res.get('main', {}).get('temp')
+
+    print("The temperature is {} \u00b0F.".format(temp))
+
+if __name__ == '__main__':
+    retrieve_temp()
--- main_unit_tests.py
+++ main_unit_tests.py
@@ -1,14 +1,22 @@
+import io
 import json
+import sys
 import unittest
 
 from unittest.mock import patch
 import main
 
-#I don't really like depending on apis for unit tests,
+#I don't really like depending on external apis for unit tests,
 # so I copied text from response.text to use for api responses.
 GOOD_RESPONSE = '{"coord":{"lon":-93.6,"lat":41.59},"weather":[{"id":801,"main":"Clouds","description":"few clouds","icon":"02d"}],"base":"stations","main":{"temp":266.05,"pressure":1020,"humidity":57,"temp_min":265.15,"temp_max":267.15},"visibility":16093,"wind":{"speed":5.1,"deg":250},"clouds":{"all":20},"dt":1518380100,"sys":{"type":1,"id":865,"message":0.0039,"country":"US","sunrise":1518354822,"sunset":1518392646},"id":4853828,"name":"Des Moines","cod":200}'
 INVALID_API_KEY_RESPONSE = '{"cod":401, "message": "Invalid API key. Please see http://openweathermap.org/faq#error401 for more info."}'
 INVALID_CITY_RESPONSE = '{"cod":"404","message":"city not found"}'
+
+#This class is for mocking when a function uses requests.get()
+# so that tests don't need to actually hit an endpoint.
+class requests_mock:
+    def json():
+        return json.loads(GOOD_RESPONSE)
 
 class TestMain(unittest.TestCase):
     def test_json_response_200(self):
@@ -25,6 +33,11 @@ class TestMain(unittest.TestCase):
     def test_json_response_404(self):
         jsn = json.loads(INVALID_CITY_RESPONSE)
         err = "Api call unsuccessful. Code: 404 Error Message: city not found"
+        self.assertRaisesRegex(LookupError, err, main.validate_json_response, jsn)
+
+    def test_response_missing_temp(self):
+        jsn = json.loads('{"cod":200}')
+        err = "Temperature was not found in response."
         self.assertRaisesRegex(LookupError, err, main.validate_json_response, jsn)
 
     @patch('main.os.environ.get')
@@ -47,6 +60,30 @@ class TestMain(unittest.TestCase):
         err = "Environmental variable retrieval failed. "
         err +="Was the API key set and config file sourced?"
         self.assertRaisesRegex(SystemExit, err, main.get_api_response, "Ames")
+
+
+    def test_validate_bad_input(self):
+        bad = '!@#$%^&*()_+-=`~[]:";<>,.12345667890'
+        err = "City is invalid. Please only use letters and spaces in city name."
+
+        for b in bad:
+            self.assertRaisesRegex(ValueError, err, main.validate_input, b)
+
+    @patch('main.requests.get')
+    @patch('main.input')
+    @patch('main.os.environ.get')
+    def test_retrieve_temp(self, envs, inp, requests_get):
+        envs.side_effect = ['Valid_Key', 'Valid_Base_Url']
+        inp.return_value = 'Valid Input'
+        output = "The temperature is 266.05 \u00b0F.\n"
+        requests_get.side_effect = [requests_mock]
+
+        std_out = io.StringIO()
+        sys.stdout = std_out
+        main.retrieve_temp()
+        sys.stdout = sys.__stdout__
+
+        self.assertEqual(output, std_out.getvalue())
 
 if __name__ == '__main__':
     unittest.main()
