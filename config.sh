65a9cd6e404dce7fd23defb6ca0dd85753961e9a
midfield99 <midfield99@users.noreply.github.com>
2018-02-12 11:08:37 -0600

    Delete config.sh
    This needs to be removed,  those environmental variables need to be set but changes are tracked if the file is in the repo.

--- config.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#config file to set up configuration details
-#Run source config.sh to set environmental config variables.
-export OWM_API_KEY="FAKE_KEY"
-export OWM_BASE_URL="http://api.openweathermap.org/data/2.5/"
